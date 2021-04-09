3b4d9b5ff0ab4d5bf2dfe99fb133a7cc28e0438f
SoumyadeepDey2002 <74682979+SoumyadeepDey2002@users.noreply.github.com>
2021-04-09 18:26:38 +0530

    Add files via upload

---
+++ Basics/1.Datatypes.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // char, int, float, double, bool
+    char c = 'a';
+    int a = 5;
+    double b = 3.5;
+    a = 4.5;
+    bool d = 0;
+    bool e = -45;   //any number other than 0 is considered true
+
+    cout<< a <<" "<< b << " " << c << " " << d << " " << e << endl;
+
+    //long int, long long int(long long)
+
+    long long big = 45467463;
+    cout<<big;
+
+    return 0;
+}
