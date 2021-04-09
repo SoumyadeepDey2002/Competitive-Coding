c26f7ef673c044e14d593e3372f7a074ee12faa3
SoumyadeepDey2002 <74682979+SoumyadeepDey2002@users.noreply.github.com>
2021-04-09 18:56:50 +0530

    Add files via upload

---
+++ Basics/2 Basic IO.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    double b;
+    char c;
+    cin >> a >> b >> c;
+    cout << a << " " << b << " " << c << endl;
+
+    // cin keeps taking data in a variable until it encounters ' ' or '\n'
+
+    // However char always take inside a single thing  like if input 123 it will take 1 the next char
+    // will take 2
+
+    return 0;
+}
